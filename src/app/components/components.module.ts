ea934c0d12ff20e539d58e6f798a121387462c17
fzanfolim <felipezanfolim@saofrancisco.com.br>
2023-01-17 22:27:10 -0300

    chore: component error message

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DynamicErrorComponent } from './dynamic-form/dynamic-error/dynamic-error.component';
+import { DynamicErrorComponent } from './dynamic-error/dynamic-error.component';
 import { DynamicFieldComponent } from './dynamic-field/dynamic-field.component';
 import { DynamicInputComponent } from './dynamic-field/dynamic-input/dynamic-input.component';
 import { DynamicSelectComponent } from './dynamic-field/dynamic-select/dynamic-select.component';
